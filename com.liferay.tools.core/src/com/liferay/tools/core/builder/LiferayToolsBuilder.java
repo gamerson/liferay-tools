7f98ff6636f655a36ac6a0ba3f5e3a828314482d
Gregory Amerson <gregory.amerson@liferay.com>
2016-06-28 16:56:15 -0500

    added com.liferay.tools.core bundle

---
+++ com.liferay.tools.core/src/com/liferay/tools/core/builder/LiferayToolsBuilder.java
@@ -0,0 +1,61 @@
+package com.liferay.tools.core.builder;
+
+import java.util.List;
+import java.util.Map;
+
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResourceDelta;
+import org.eclipse.core.resources.IncrementalProjectBuilder;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.IProgressMonitor;
+
+import com.liferay.source.formatter.SourceFormatter;
+import com.liferay.source.formatter.SourceFormatterArgs;
+
+@SuppressWarnings("restriction")
+public class LiferayToolsBuilder extends IncrementalProjectBuilder {
+
+	public final static String ID = "com.liferay.tools.core.builder";
+
+	@Override
+	protected IProject[] build(int kind, Map<String, String> args, IProgressMonitor monitor) throws CoreException {
+		final IProject project = getProject();
+
+		final boolean fullBuild = kind == FULL_BUILD || kind == CLEAN_BUILD;
+
+		if (fullBuild) {
+			format(getProject().getLocation());
+		} else {
+			final IResourceDelta delta = getDelta(project);
+
+		}
+
+		return null;
+	}
+
+	private void format(IPath location) {
+
+		new SourceFormatterArgs();
+
+		SourceFormatterArgs sourceFormatterArgs = new SourceFormatterArgs();
+
+		String baseDirName = location.toOSString();
+
+		sourceFormatterArgs.setAutoFix(false);
+		sourceFormatterArgs.setBaseDirName(baseDirName);
+		sourceFormatterArgs.setPrintErrors(true);
+		sourceFormatterArgs.setThrowException(false);
+		sourceFormatterArgs.setUseProperties(false);
+
+		SourceFormatter sourceFormatter = new SourceFormatter(sourceFormatterArgs);
+
+		try {
+			sourceFormatter.format();
+			List<String> errors = sourceFormatter.getErrorMessages();
+		} catch (Exception e) {
+		}
+
+	}
+
+}
---
+++ com.liferay.tools.core/src/com/liferay/tools/core/builder/LiferayToolsNature.java
@@ -0,0 +1,65 @@
+package com.liferay.tools.core.builder;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.core.resources.ICommand;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IProjectDescription;
+import org.eclipse.core.resources.IProjectNature;
+import org.eclipse.core.runtime.CoreException;
+
+public class LiferayToolsNature implements IProjectNature {
+
+	private IProject project;
+
+    @Override
+    public IProject getProject() {
+        return project;
+    }
+
+    @Override
+    public void setProject(IProject project) {
+        this.project = project;
+    }
+    
+    @Override
+    public void configure() throws CoreException {
+        final IProjectDescription desc = project.getDescription();
+        addBuilder(desc);
+    }
+
+    @Override
+    public void deconfigure() throws CoreException {
+        IProjectDescription desc = project.getDescription();
+        removeBuilder(desc);
+    }
+    
+    private static void addBuilder(IProjectDescription desc) {
+        ICommand[] commands = desc.getBuildSpec();
+        for (ICommand command : commands) {
+            if (command.getBuilderName().equals(LiferayToolsBuilder.ID))
+                return;
+        }
+
+        ICommand[] nu = new ICommand[commands.length + 1];
+        System.arraycopy(commands, 0, nu, 0, commands.length);
+
+        ICommand command = desc.newCommand();
+        command.setBuilderName(LiferayToolsBuilder.ID);
+        nu[commands.length] = command;
+        desc.setBuildSpec(nu);
+    }
+
+    private static void removeBuilder(IProjectDescription desc) {
+        ICommand[] commands = desc.getBuildSpec();
+        List<ICommand> nu = new ArrayList<>();
+        for (ICommand command : commands) {
+            if (!command.getBuilderName().equals(LiferayToolsBuilder.ID)) {
+                nu.add(command);
+            }
+        }
+        desc.setBuildSpec(nu.toArray(new ICommand[0]));
+    }
+
+}
